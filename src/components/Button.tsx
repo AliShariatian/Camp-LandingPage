2afb938e69e522a0440173e6d471edd96e65d030
Ali Shariatian <shariatian92@gmail.com>
2024-02-29 00:01:06 +0330

    add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import { FC } from "react";
+
+type Props = {
+   type: "button" | "submit";
+   variant: "btn_dark_green";
+   title: string;
+   icon?: string;
+};
+
+const Button: FC<Props> = ({ type, title, icon, variant }): JSX.Element => {
+   return (
+      <button type={type} className={`${variant} flexCenter gap-3 rounded-full border`}>
+         {icon && <Image src={icon} alt={title} width={24} height={24} />}
+         <label className="bold-16 whitespace-nowrap">{title}</label>
+      </button>
+   );
+};
+
+export default Button;
