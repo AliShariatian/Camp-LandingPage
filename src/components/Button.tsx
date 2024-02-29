60d4661a34e64b71197c305098b07e18b5d9268e
Ali Shariatian <shariatian92@gmail.com>
2024-02-29 18:40:19 +0330

    feat: add GetApp section into view

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,11 +6,12 @@ type Props = {
    variant: string;
    title: string;
    icon?: string;
+   full?: boolean;
 };
 
-const Button: FC<Props> = ({ type, title, icon, variant }): JSX.Element => {
+const Button: FC<Props> = ({ type, title, icon, variant, full }): JSX.Element => {
    return (
-      <button type={type} className={`${variant} flexCenter gap-3 rounded-full border`}>
+      <button type={type} className={`${variant} ${full && "w-full"} flexCenter gap-3 rounded-full border`}>
          {icon && <Image src={icon} alt={title} width={24} height={24} />}
          <label className="bold-16 cursor-pointer whitespace-nowrap">{title}</label>
       </button>
